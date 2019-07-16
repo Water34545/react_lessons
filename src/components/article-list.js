34945aba986f3ee276b95ccf1373d14423477c12
A <a@As-MacBook-Air.local>
2019-07-16 21:29:07 +0300

    start lesson 1

---
+++ src/components/article-list.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Article from "./article";
+import useAccordion from "../custom-hooks/accordion";
+
+export default function ArticleList({ articles }) {
+  const { openItemId, toggleOpenItem } = useAccordion();
+
+  const articleItems = articles.map(article => (
+    <li key={article.id}>
+      <Article
+        article={article}
+        onBtnClick={toggleOpenItem(article.id)}
+        isOpen={article.id === openItemId}
+      />
+    </li>
+  ));
+
+  return <ul>{articleItems}</ul>;
+}
+
+//import AccodrionComponent from './accordion-component'
+/*
+import accordion from '../decorators/accordion'
+
+function ArticleList({ articles, toggleOpenArticle, openArticleId }) {
+    const articleItems = articles.map((article) => (
+      <li key={article.id}>
+        <Article
+          article={article}
+          onBtnClick={toggleOpenArticle(article.id)}
+          isOpen={article.id === openArticleId}
+        />
+      </li>
+    ))
+
+    return <ul>{articleItems}</ul>
+}
+
+export default accordion(ArticleList)
+*/
---
+++ src/decorators/accordion.js
@@ -0,0 +1,21 @@
+//HOC === Higher Order Component == Decorator
+import React from "react";
+
+export default OriginalComponent =>
+  class DecoratedComponent extends React.Component {
+    state = {
+      openItemId: null
+    };
+
+    toggleOpenItem = openItemId => () => this.setState({ openItemId });
+
+    render() {
+      return (
+        <OriginalComponent
+          {...this.props}
+          openItemId={this.state.openItemId}
+          toggleOpenItem={this.toggleOpenItem}
+        />
+      );
+    }
+  };
